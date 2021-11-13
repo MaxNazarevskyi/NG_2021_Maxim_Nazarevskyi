3771cb8c517e9d3ca284126d449ee83d504207ec
MaxNazarevskyi <ithefirstly@gmail.com>
2021-11-13 21:52:05 +0200

    Language changed

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -17,17 +17,17 @@ cin>>c;
 cout<<"\n";
 b1=pow(b,2);
 D=b1-4*a*c;
-cout<<"Дискриминант равен: "<<D<<"\n";
-if(D>0)cout<< "\nДва корня";
-if(D==0)cout<< "\nОдин корень";
-if(D<0)cout<< "\nКорней нет";
+cout<<"D=: "<<D<<"\n";
+if(D>0)cout<< "\nTwo answers";
+if(D==0)cout<< "\nOne answer";
+if(D<0)cout<< "\nNo answer";
 cout<<"\n";
 sqD=sqrt(D);
 x1=(-b+sqD)/2*a;
 x2=(-b-sqD)/2*a;
 x=(-b)/2*a;
-if(D>0)cout<<"Первый корень: "<<x1<<"\n"<<"Второй корень: "<<x2<<endl;
-if(D==0)cout<<"Корень равняется :"<<x;
+if(D>0)cout<<"First answer: "<<x1<<"\n"<<"Second answer: "<<x2<<endl;
+if(D==0)cout<<"Answer :"<<x;
 
     return 0;
 }
