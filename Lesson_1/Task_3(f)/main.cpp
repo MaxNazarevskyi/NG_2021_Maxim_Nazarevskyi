9d8be55eab5129256c00e8fdedcc34bc7d4a9d03
MaxNazarevskyi <ithefirstly@gmail.com>
2021-12-28 00:15:47 +0200

    Added Lesson 2

--- Lesson_1/Task_3(f)/main.cpp
+++ Lesson_1/Task_3(f)/main.cpp
@@ -6,8 +6,10 @@ int main()
 {
     int a =20;
     int b =50;
-    a = b, b = a;
+    b=a+b; //b=70
+    a=b-a; //a=70-20=50
+    b=b-a; //b=70-50=20
     cout << "a=" << a << endl;
-    cout << "b=" << b << endl;
+    cout << "b=" << b;
     return 0;
 }
