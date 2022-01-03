8f7e2ec691695e3ebd70551d8eadaf64ca501bb0
MaxNazarevskyi <ithefirstly@gmail.com>
2022-01-03 22:46:16 +0200

    Added Lesson_3/Task_1

---
+++ Lesson_3/Task_1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a [10];
+    for (int i = 0; i < 10; i++) {
+            cout << "[" << i + 1 << "]: ";
+            cin >> a[i];
+    }
+    cout << "\nMassive: ";
+        for (int i = 0; i < 10; ++i) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+{
+    int b=0;
+    cout << "Your number = ";
+    cin >> b;
+    cout << "Updated massive: ";
+    for (int j = 0; j < 10; ++j) {
+        cout << b+a[j] << " ";
+}
+    return 0;
+}
+}
+
+
