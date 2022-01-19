2e8de50d77c603182438b4c95d22adfdd3a1f826
MaxNazarevskyi <ithefirstly@gmail.com>
2022-01-20 01:03:42 +0200

    Updated two tasks

--- Lesson_3/Task_1/main.cpp
+++ Lesson_3/Task_1/main.cpp
@@ -5,25 +5,25 @@ using namespace std;
 int main()
 {
     int a [10];
-    for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             cout << "[" << i + 1 << "]: ";
             cin >> a[i];
     }
     cout << "\nMassive: ";
         for (int i = 0; i < 10; ++i) {
             cout << a[i] << " ";
-        }
-        cout << endl;
+    }
+    cout << endl;
     {
-    int b = 0;
-    cout << "Your number = ";
-    cin >> b;
-    cout << "Updated massive: ";
-        for (int j = 0; j < 10; ++j) {
-            cout << b + a[j] << " ";
+       int b = 0;
+       cout << "Your number = ";
+       cin >> b;
+       cout << "Updated massive: ";
+           for (int j = 0; j < 10; ++j) {
+               cout << b + a[j] << " ";
         }
         return 0;
     }
-  }
+}
 
 
--- Lesson_4/Task_2/main.cpp
+++ Lesson_4/Task_2/main.cpp
@@ -8,12 +8,14 @@ int main()
     char F_str[255] = "";
     char S_str[255] = "";
     cin.getline(F_str, 255);
-    cin.getline(S_str, 255);
+    cin.get(S_str, 255);
     for (int i = 0; i < 255; i++) {
-        if (F_str[i] != S_str[i])
+        if (F_str[i] != S_str[i]){
             same = false;
+            break;
+        }
     }
-    if (same == true)
+    if (same)
         cout << "First string and Second string are the same";
     else
         cout << "First string and Second string are NOT same";
